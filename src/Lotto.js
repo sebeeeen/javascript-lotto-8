5a48370ea7b0f62183eb8c5d1844ce09d62a1ba3
Sebeen Kwon <a856412@gmail.com>
2025-11-03 21:55:37 +0900

    feat: Lotto 클래스 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -3,16 +3,48 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = [...numbers].sort((a, b) => a - b);
   }
 
   #validate(numbers) {
+    this.#validateLength(numbers);
+    this.#validateDuplicate(numbers);
+    this.#validateRange(numbers);
+  }
+
+  #validateLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
 
-  // TODO: 추가 기능 구현
+  #validateDuplicate(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+  }
+
+  #validateRange(numbers) {
+    const hasInvalidNumber = numbers.some(number => number < 1 || number > 45);
+    if (hasInvalidNumber) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+  }
+
+  getNumbers() {
+    return this.#numbers;
+  }
+
+  countMatches(winningNumbers) {
+    return this.#numbers.filter(number => 
+      winningNumbers.includes(number)
+    ).length;
+  }
+
+  hasBonus(bonusNumber) {
+    return this.#numbers.includes(bonusNumber);
+  }
 }
 
 export default Lotto;
